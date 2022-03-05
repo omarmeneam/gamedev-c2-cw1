13d192cf1b6ceac323774c9432acd6b1cd73507d
Omar <o.meneam@gmail.com>
2022-03-05 22:53:01 +0300

    c2-cw1

---
+++ c2-cw1/c2-cw1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace c2_cw1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string characterName = "The Witcher";
+            string superpower1 = "Mind Control";
+            string superpower2 = "Object Manipulation";
+            int age = 1000;
+            float speed = 200;
+
+            Console.WriteLine("My name is {0}, My superpower1 is {1}, {2} , My age is {3} and my speed is {4}", characterName, superpower1, superpower2, age, speed);
+        }
+    }
+}
